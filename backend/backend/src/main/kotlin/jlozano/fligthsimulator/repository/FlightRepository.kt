c65328e84503d47526a191e44e61dc10946ff801
Julian Lozano <jalozanog93@hotmail.com>
2026-04-23 20:16:38 -0500

    [backend-boot] Changes to solve conf, and interface impl mistake

--- backend/backend/src/main/kotlin/jlozano/fligthsimulator/repository/FlightRepository.kt
+++ backend/backend/src/main/kotlin/jlozano/fligthsimulator/repository/FlightRepository.kt
@@ -1,11 +1,8 @@
 package jlozano.fligthsimulator.repository
 
 import jlozano.fligthsimulator.domain.Flight
-import jlozano.fligthsimulator.domain.FlightMetric
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface FlightRepository : JpaRepository<Flight, UUID> {
-    fun findAllByFlightIdOrderByTimestampAsc(flightId: UUID): List<FlightMetric>
-    fun findTopByFlightIdOrderByTimestampDesc(flightId: UUID): FlightMetric?
 }
--- backend/backend/src/test/kotlin/jlozano/fligthsimulator/BackendApplicationTests.kt
+++ backend/backend/src/test/kotlin/jlozano/fligthsimulator/BackendApplicationTests.kt
@@ -2,8 +2,10 @@ package jlozano.fligthsimulator
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
     @Test
